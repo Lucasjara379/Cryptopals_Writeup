cb761ea6c6917e5eb5f3e9bcb7c7b871b809c172
Lucas Jarana <jarlucas56@gmail.com>
2025-11-01 16:52:33 +0000

    filter decrypted_msgs to have bytes that are the same size as input bytes and only have printable characters in ascii.

---
+++ set1/challenge3.py
@@ -0,0 +1,20 @@
+def decrypt(hex_str):
+    """Decrypt hex string that has been encrypted with a single byte xor."""
+
+    raw_bytes = bytes.fromhex(hex_str)
+    decrypted_msgs = []
+
+    for i in range(256):
+        xored = bytes([b ^ i for b in raw_bytes if b ^ i in range(32, 128)])
+        if len(xored) == len(raw_bytes):
+            msg = xored.decode()
+            decrypted_msgs.append(msg)
+
+    return decrypted_msgs
+
+
+if __name__ == '__main__':
+    m = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+    print(decrypt(m))
+
+# key is 88 and message is "Cooking MC's like a pound of bacon"
